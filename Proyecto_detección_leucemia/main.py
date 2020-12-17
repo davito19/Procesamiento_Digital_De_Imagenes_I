a5cb410b2162eb172e3306ee54f02e2777f7dc95
Davito19 <omardbonett@gmail.com>
2020-12-16 19:15:53 -0500

    agregando un boton

--- Proyecto_detección_leucemia/main.py
+++ Proyecto_detección_leucemia/main.py
@@ -1,7 +1,31 @@
 import tkinter as Tk
 from tkinter import messagebox
 from tkinter import filedialog
+"""
+import os
+import numpy as np
+from keras.preprocessing.image import load_img, img_to_array
+from keras.models import load_model
 
+size = (640, 480)
+model = os.getcwd() + "/Modelo/leucemiamodel.h5"
+height = os.getcwd() + "/Modelo/leucemiamodel.h5"
+cnn = load_model(model)
+cnn.load_weights(height)
+
+file1 = os.getcwd() + '/Data/Validacion/sanas/BloodImage_00340.jpg'
+file0 = os.getcwd() + '/Data/Validacion/leucemia/FB_IMG_1582862764903.jpg'
+x = load_img(file1, target_size=size)
+x = img_to_array(x)
+x = np.expand_dims(x, axis=0)
+array = cnn.predict(x)
+result = array[0]
+print(result)icon.ico
+if result == 0:
+    print("pred: Leucemia")
+else:
+    print("pred: Sanas")
+"""
 
 # Definimos la raiz
 raiz = Tk.Tk()
@@ -31,7 +55,7 @@ def archivo(op):
         print(fichero)
     elif op == "guardar como":
         filename = filedialog.asksaveasfilename(initialdir="/", title="Select file",
-                                                     filetypes=(("jpeg files", "*.jpg"), ("all files", "*.*")))
+                                                filetypes=(("jpeg files", "*.jpg"), ("all files", "*.*")))
         print(filename)
     elif op == "salir":
         valor = messagebox.askyesno("Salir", "¿Desea salir?")
@@ -64,4 +88,13 @@ help_menu.add_command(label="Acerca de", command=lambda: info("Acerca de"))
 Barra_Menu.add_cascade(label="Archivo", menu=file_menu)
 Barra_Menu.add_cascade(label="ayuda", menu=help_menu)
 
+abrir = Tk.Button(frame, text="Abrir Archivo", command=lambda :archivo("abrir"))
+abrir.config(padx=15, pady=10, justify="center")
+abrir.grid(row=0,column=0)
+
 raiz.mainloop()
+"""
+
+import tensorflow as tf
+tf.config.experimental.list_physical_devices('GPU')
+"""
