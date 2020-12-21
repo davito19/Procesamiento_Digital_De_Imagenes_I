ae4d70a68d53349844e9f6f2b08d1d922eb664f2
Davito19 <omardbonett@gmail.com>
2020-12-21 10:38:58 -0500

    Update main.py
    añadiendo cometarios finales

--- Proyecto_detección_leucemia/main.py
+++ Proyecto_detección_leucemia/main.py
@@ -21,6 +21,7 @@ from PIL import ImageTk, Image  # Modulos utilizados para procesar imagenes
 from keras.preprocessing.image import load_img, img_to_array  # Modulos utilizados para procesar imagenes
 from keras.models import load_model  # Modulo para cargar modelos de Deep Learning
 
+
 # --------------------------------------------------------------------------
 # -- 2. Definición de Variables Globales del juego   -----------------------
 # --------------------------------------------------------------------------
@@ -46,32 +47,59 @@ frame.pack(fill="both", expand="True")  # Empaquetamps el frame en la ventana
 fichero = 'No.png'  # Ruta de la imagen a mostrar una vez inicie el programa
 hay = False  # Me dice si se cargo una imagen o no
 
-
 # --------------------------------------------------------------------------
 # -- 3. Definición de funciones del juego   --------------------------------
 # --------------------------------------------------------------------------
 
 def clasificacion(path, bole):
-    if bole:
-        size = (640, 480)
+    """
+    Funnción que permite determinar si una muestra es leucemia o esta sanas
 
-        x = load_img(path, target_size=size)
-        x = img_to_array(x)
+    Parameters
+    ----------
+    path : String
+        ruta de la imagen a clasificar.
+    bole : bool
+        variable swicthe que permite hacer la clasificación.
+
+    Returns
+    -------
+    int
+        retorna un valor entero para cada uno de los casos dados.
+
+    """
+    if bole:
+        size = (640, 480) # Tamaño de la imagen
+
+        x = load_img(path, target_size=size) # Se carga la imagen
+        x = img_to_array(x) 
         x = np.expand_dims(x, axis=0)
-        array = cnn.predict(x)
-        result = array[0]
-        print(result)
-        if result == 0:
+        array = cnn.predict(x) # Realizamos la predicción
+        result = array[0] # extraemos la clase
+        if result == 0: # Retornamos la clasificación corrrespondiente
             print("pred: Leucemia")
             return 1
         else:
             print("pred: Sanas")
             return 2
-    else:
+    else: # retornamos 3 para indicar que no se clasifico nada
         return 3
 
 
 def info(adicional):
+    """
+    funcion para el menu de ayuda de la ventana
+
+    Parameters
+    ----------
+    adicional : String
+        la opción a desplegar en la ventana emergente
+
+    Returns
+    -------
+    None.
+
+    """
     if adicional == "Acerca de":
         messagebox.showinfo("Acerda de", "Clasificador de Leucemia Mieloide Versión 1 \npor: davito, manuvlj")
     elif adicional == "Licencia":
@@ -81,20 +109,38 @@ def info(adicional):
 
 
 def archivo(op):
-    global mostrar
-    global fichero
-    global hay
+    """
+    funcion para ejecutar las opciones de los botones en la interfaz grafica 
+    y la barrade menu de archivo
 
-    if op == "abrir":
+    Parameters
+    ----------
+    op : string
+        opción a realizar
+
+    Returns
+    -------
+    None.
+
+    """
+    
+    global mostrar # Variable Goblal para mostrar una imagen en la ventana
+    global fichero # variable global del path de la imagen a mostrar
+    global hay # Variable booleaa que permite hacer la clasificación
+
+    if op == "abrir": # Sí la opcion es abrir
+        # abre una ventana emergente y se guarda el path del archivo selecionado
         fichero = filedialog.askopenfilename(title="Abrir", initialdir="./",
                                              filetypes=(("Todos los Archivos", "*.*"),
                                                         ("Archivos formato PNG", "*.png"),
                                                         ("Archivos formato JPG", "*.jpg")))
         try:
+            # Sí el archivo existe lo muestra y activa la clasificación
             mostrar = ImageTk.PhotoImage(Image.open(fichero).resize((240, 240)))
             mostrar_image.config(image=mostrar)
             hay = True
         except:
+            # si el archivo no existe 
             fichero = 'No.png'
             mostrar = ImageTk.PhotoImage(Image.open(fichero).resize((240, 240)))
             mostrar_image.config(image=mostrar)
@@ -103,22 +149,22 @@ def archivo(op):
     elif op == "clasi":
         clasi = clasificacion(fichero, hay)
         if clasi == 1:
-            resultado.config(text="Tenes leucemia")
+            resultado.config(text="La muestra presenta \n celulas de leucemia mieloide")
         elif clasi == 2:
-            resultado.config(text="Estas sano")
+            resultado.config(text="La muestra presenta \n globulos blancos normales")
         else:
             resultado.config(text="No hay imagen a clasificar")
-    # elif op == "guardar como":
-    #     filename = filedialog.asksaveasfilename(initialdir="/", title="Select file",
-    #                                             filetypes=(("jpeg files", "*.jpg"), ("all files", "*.*")))
-
+                                           
     elif op == "salir":
         valor = messagebox.askyesno("Salir", "¿Desea salir?")
         if valor:
             raiz.destroy()
 
 
-# Definimos el frame que estará dentro de la raiz
+
+# --------------------------------------------------------------------------
+# -- 4. widget de la interfaz grafica   ------------------------------------
+# --------------------------------------------------------------------------
 
 
 # -------------------------------barra menu--------------------------------- #
@@ -159,5 +205,9 @@ mostrar_image.place(x=10, y=100)
 resultado = Tk.Label(frame, text='No se ha clasificado', cursor="dot")
 resultado.config(padx=15, pady=10, justify="center")
 resultado.place(x=300, y=220)
+
+# --------------------------------------------------------------------------
+# -- 5. bucle infinito para que el programa funcione   ---------------------
+# --------------------------------------------------------------------------
 
 raiz.mainloop()
